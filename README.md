7b2405f1a78046aaf6fe3d474fefd6f3f8d1e1bf
Damian Wolgast <damian@si-co.net>
2021-03-16 18:35:28 +0100

    Test passes

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Solutions for Katas listed on [http://codekata.com/].
 
 Because experience is the only teacher ;-)
+
+While coding I am tryying to avoid using helpers of the framework, e.g., LINQ.
---
+++ src/Kata04/Kata04.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace CodeKata.Kata04.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            // Arrange
+
+            // Act
+            var weatherdata = DataMunging.GetSmallestSpread();
+
+            // Assert
+            Assert.Equal(14, weatherdata.Day);
+            Assert.Equal(2, weatherdata.TemperaturSpread);
+        }
+    }
+}
