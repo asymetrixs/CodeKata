6f33f5054747c3308a6f342f2fe6c8e6d450de31
Damian Wolgast <damian@si-co.net>
2021-03-19 11:18:16 +0100

    Kata08

---
+++ src/Kata08/Kata08.Tests/UnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace CodeKata.Kata08.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void TestSimple()
+        {
+            // Arrange
+            IEnumerable<string> list = new List<string>() { "abc", "def", "word1", "word2", "abcdef", "hijklm", "nopqr" };
+
+            // Act
+            var actualWords = Kata.GetSixOfTwo(list);
+
+            // Assert
+            Assert.Contains(actualWords, aw => aw.Item1 == "abc" && aw.Item2 == "def" || aw.Item2 == "abc" && aw.Item1 == "def");
+            Assert.Single(actualWords);
+        }
+
+        [Fact]
+        public void TestComlex()
+        {
+            // Arrange
+            IEnumerable<string> list = File.ReadAllLines("wordlist.txt");
+
+            // Act
+            var actualWords = Kata.GetSixOfTwo(list);
+
+            // Assert
+            Assert.Contains(actualWords, aw => aw.Item1 == "al" && aw.Item2 == "bums" || aw.Item2 == "al" && aw.Item1 == "bums");
+            Assert.Contains(actualWords, aw => aw.Item1 == "bar" && aw.Item2 == "ely" || aw.Item2 == "bar" && aw.Item1 == "ely");
+            Assert.Contains(actualWords, aw => aw.Item1 == "be" && aw.Item2 == "foul" || aw.Item2 == "be" && aw.Item1 == "foul");
+            Assert.Contains(actualWords, aw => aw.Item1 == "con" && aw.Item2 == "vex" || aw.Item2 == "con" && aw.Item1 == "vex");
+            Assert.Contains(actualWords, aw => aw.Item1 == "here" && aw.Item2 == "by" || aw.Item2 == "here" && aw.Item1 == "by");
+            Assert.Contains(actualWords, aw => aw.Item1 == "jig" && aw.Item2 == "saw" || aw.Item2 == "jig" && aw.Item1 == "saw");
+            Assert.Contains(actualWords, aw => aw.Item1 == "tail" && aw.Item2 == "or" || aw.Item2 == "tail" && aw.Item1 == "or");
+            Assert.Contains(actualWords, aw => aw.Item1 == "we" && aw.Item2 == "aver" || aw.Item2 == "we" && aw.Item1 == "aver");
+        }
+    }
+}
---
+++ src/Kata08/Kata08/Kata.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeKata.Kata08
+{
+    public static class Kata
+    {
+        public static IEnumerable<(string, string)> GetSixOfTwo(IEnumerable<string> list)
+        {
+            // Prefilter list to only contain 6 letter words and shorter
+            // This way we do not need to scan the whole list every time
+            // And due to the hashmap have fast access to elements
+            // We have to use ToLower so that a word starting with a capital
+            // Letter will still be recognized in a 6 character word
+            var filteredList = list
+                .Where(l => l.Length <= 6)
+                .Select(l => l.ToLowerInvariant())
+                .ToHashSet();
+
+            // Concurrent bag to add result entries concurrently
+            var resultSet = new ConcurrentBag<(string, string)>();
+
+            Parallel.ForEach(list, (item) =>
+            {
+                // Check if item has length of 6
+                if (item.Length != 6)
+                {
+                    return;
+                }
+
+                // Split item into (first+last): 1+5, 2+4, 3+3, 4+2, 5+1 and search for each part
+                for (int first = 1; first < 6; first++)
+                {
+                    var firstPart = item[0..first];
+                    var lastPart = item[first..6];
+
+                    if (filteredList.Contains(firstPart) && filteredList.Contains(lastPart))
+                    {
+                        resultSet.Add((firstPart, lastPart));
+                    }
+                }
+
+            });
+
+            // Return bag
+            return resultSet.AsEnumerable();
+        }
+    }
+}
