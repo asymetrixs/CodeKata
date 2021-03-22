1f8fbb22342e32e0bb401ae1c514c9175570d050
Damian Wolgast <damian@si-co.net>
2021-03-22 17:11:55 +0100

    * Initial commit * Add Nothing, add 10

---
+++ src/Kata11/Kata11.Tests/UnitTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+
+namespace CodeKata.Kata11.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void SortBallsAddNothingTest()
+        {
+            // Arrange
+            var sortedList = new int[] { };
+            var expectedSorted = new int[] { };
+
+            // Act
+            var actualSorted = Kata11.AddAndSort(sortedList, null);
+
+            // Assert
+            Assert.Equal(expectedSorted, actualSorted);
+        }
+
+        [Fact]
+        public void SortBallsAdd20Test()
+        {
+            // Arrange
+            var sortedList = new int[] { };
+            var expectedSorted = new int[] { 20 };
+
+            // Act
+            var actualSorted = Kata11.AddAndSort(sortedList, 20);
+
+            // Assert
+            Assert.Equal(expectedSorted, actualSorted);
+        }
+
+        [Fact]
+        public void SortBallsAdd10Test()
+        {
+            // Arrange
+            var sortedList = new int[] { 20 };
+            var expectedSorted = new int[] { 10, 20 };
+
+            // Act
+            var actualSorted = Kata11.AddAndSort(sortedList, 10);
+
+            // Assert
+            Assert.Equal(expectedSorted, actualSorted);
+        }
+
+        [Fact]
+        public void SortBallsAdd30Test()
+        {
+            // Arrange
+            var sortedList = new int[] { 10, 20 };
+            var expectedSorted = new int[] { 10, 20, 30 };
+
+            // Act
+            var actualSorted = Kata11.AddAndSort(sortedList, 30);
+
+            // Assert
+            Assert.Equal(expectedSorted, actualSorted);
+        }
+    }
+}
---
+++ src/Kata11/Kata11/Kata11.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace CodeKata.Kata11
+{
+    public class Kata11
+    {
+        public static int[] AddAndSort(int[] sortedList, int? number)
+        {
+            if(number is not null)
+            {
+                var tmp = sortedList.ToList();
+
+                for (int i = 0; i < sortedList.Length; i++)
+                {
+                    if(sortedList[i] > number)
+                    {
+                        tmp.Insert(i, number.Value);
+                    }
+                }
+
+                sortedList = tmp.ToArray();
+            }
+
+            return sortedList;
+        }
+    }
+}
