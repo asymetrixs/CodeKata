7fcad819c25fcf8952f20f6cae49aa9c032fcc6a
Damian Wolgast <damian@si-co.net>
2021-03-22 17:13:31 +0100

    Add 20 and 30

--- src/Kata11/Kata11/Kata11.cs
+++ src/Kata11/Kata11/Kata11.cs
@@ -10,12 +10,19 @@ namespace CodeKata.Kata11
             {
                 var tmp = sortedList.ToList();
 
+                bool added = false;
                 for (int i = 0; i < sortedList.Length; i++)
                 {
                     if(sortedList[i] > number)
                     {
                         tmp.Insert(i, number.Value);
+                        added = true;
                     }
+                }
+
+                if(!added)
+                {
+                    tmp = tmp.Append(number.Value).ToList();
                 }
 
                 sortedList = tmp.ToArray();
