30966285f88d604711cc31361b8fb55cef1db66a
Damian Wolgast <damian@si-co.net>
2021-03-24 16:20:16 +0100

    Added tests for B, C, D, E, F

--- src/Kata18/Kata18.Tests/UnitTests.cs
+++ src/Kata18/Kata18.Tests/UnitTests.cs
@@ -4,6 +4,19 @@ namespace CodeKata.Kata18.Tests
 {
     public class UnitTests
     {
+        private DependencyTree _dependencyTree;
+
+        public UnitTests()
+        {
+            this._dependencyTree = new DependencyTree();
+            this._dependencyTree.Add('A', new char[] { 'B', 'C' });
+            this._dependencyTree.Add('B', new char[] { 'C', 'E' });
+            this._dependencyTree.Add('C', new char[] { 'G' });
+            this._dependencyTree.Add('D', new char[] { 'A', 'F' });
+            this._dependencyTree.Add('E', new char[] { 'F' });
+            this._dependencyTree.Add('F', new char[] { 'H' });
+        }
+
         [Fact]
         public void AddComponent()
         {
@@ -21,14 +34,7 @@ namespace CodeKata.Kata18.Tests
         public void ResolveNonExisting()
         {
             // Arrange
-            var dependencyTree = new DependencyTree();
-            dependencyTree.Add('A', new char[] { 'B', 'C' });
-            dependencyTree.Add('B', new char[] { 'C', 'E' });
-            dependencyTree.Add('C', new char[] { 'G' });
-            dependencyTree.Add('D', new char[] { 'A', 'F' });
-            dependencyTree.Add('E', new char[] { 'F' });
-            dependencyTree.Add('F', new char[] { 'H' });
-
+            var dependencyTree = _dependencyTree;
             var expectedResult = new char[] { };
 
             // Act
@@ -43,18 +49,81 @@ namespace CodeKata.Kata18.Tests
         public void ResolveA()
         {
             // Arrange
-            var dependencyTree = new DependencyTree();
-            dependencyTree.Add('A', new char[] { 'B', 'C' });
-            dependencyTree.Add('B', new char[] { 'C', 'E' });
-            dependencyTree.Add('C', new char[] { 'G' });
-            dependencyTree.Add('D', new char[] { 'A', 'F' });
-            dependencyTree.Add('E', new char[] { 'F' });
-            dependencyTree.Add('F', new char[] { 'H' });
-
+            var dependencyTree = _dependencyTree;
             var expectedResult = new char[] { 'B', 'C', 'E', 'F', 'G', 'H' };
 
             // Act
             var actualResult = dependencyTree.ResolveDependenciesOf('A');
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ResolveB()
+        {
+            // Arrange
+            var dependencyTree = _dependencyTree;
+            var expectedResult = new char[] { 'C', 'E', 'F', 'G', 'H' };
+
+            // Act
+            var actualResult = dependencyTree.ResolveDependenciesOf('B');
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ResolveC()
+        {
+            // Arrange
+            var dependencyTree = _dependencyTree;
+            var expectedResult = new char[] { 'G' };
+
+            // Act
+            var actualResult = dependencyTree.ResolveDependenciesOf('C');
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ResolveD()
+        {
+            // Arrange
+            var dependencyTree = _dependencyTree;
+            var expectedResult = new char[] { 'A', 'B', 'C', 'E', 'F', 'G', 'H' };
+
+            // Act
+            var actualResult = dependencyTree.ResolveDependenciesOf('D');
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ResolveE()
+        {
+            // Arrange
+            var dependencyTree = _dependencyTree;
+            var expectedResult = new char[] { 'F', 'H' };
+
+            // Act
+            var actualResult = dependencyTree.ResolveDependenciesOf('E');
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ResolveF()
+        {
+            // Arrange
+            var dependencyTree = _dependencyTree;
+            var expectedResult = new char[] { 'H' };
+
+            // Act
+            var actualResult = dependencyTree.ResolveDependenciesOf('F');
 
             // Assert
             Assert.Equal(expectedResult, actualResult);
